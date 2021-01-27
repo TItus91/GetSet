cade86914b98fb4e34dfd1b03e4d95005ba52f13
Kulp <l.kulpinski.91@gmail.com>
2021-01-27 13:39:44 +0100

    Added code

---
+++ GetSetProject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace GetSetProject
+{
+	class Entity
+	{
+		private string name;
+
+		public Entity(string _name)
+		{
+			name = _name;
+		}
+
+		public Entity()
+		{
+			name = null;
+		}
+
+		public string Name { get; set; }
+		public string Name2;
+		public string Name3
+		{
+			get
+			{
+				return name;
+			}
+
+			set
+			{
+				name = value;
+				Console.WriteLine($"set name to: {name}");
+			}
+		}
+
+		public string GetName()
+		{
+			return Name + ' ' + Name2 + ' ' + name;
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Entity czlowiek = new Entity();
+			czlowiek.Name = "Jan";
+			czlowiek.Name2 = "Tomasz";
+			czlowiek.Name3 = "Kowalski";
+			Console.WriteLine($"Hello {czlowiek.Name} {czlowiek.Name2} {czlowiek.Name3}");
+			Console.WriteLine($"Hello {czlowiek.GetName()}");
+		}
+	}
+
+	
+	
+}
